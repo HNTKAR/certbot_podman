fac7c97f7ad33ce93a17a3d6377f9de6bfd8ed28
kusari-k <45356209+kusari-k@users.noreply.github.com>
2020-01-12 14:40:47 +0900

    bug fixes

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 date >> /var/log/docker_log/certbot_log
-/usr/local/bin/certbot-auto certonly renew  --standalone
+/usr/local/bin/certbot-auto certonly renew  --standalone >> /var/log/docker_log/certbot_log
 
