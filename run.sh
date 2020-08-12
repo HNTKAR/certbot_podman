8e241677f053cff926091f718a7276397d45653a
kusari-k <18eh501@ms.dendai.ac.jp>
2020-08-12 23:16:16 +0900

    remove --dry-run

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 date >> /var/log/certbot.log
 sed -e "s/^domain://g" /usr/local/bin/setting.log | \
 	xargs -I {} sh -c \
-	'certbot certonly --standalone --dry-run --expand --register-unsafely-without-email --agree-tos -d {} >> /var/log/certbot.log && \
+	'certbot certonly --standalone --expand --register-unsafely-without-email --agree-tos -d {} >> /var/log/certbot.log && \
 	sleep 10'
+#'certbot certonly --standalone --dry-run --expand --register-unsafely-without-email --agree-tos -d {} >> /var/log/certbot.log && \
