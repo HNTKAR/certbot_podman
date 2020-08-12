3e9c4215759c20c3bfc2b16db5c06d09f2651724
kusari-k <45356209+kusari-k@users.noreply.github.com>
2020-08-12 17:15:48 +0900

    fix run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
 date >> /var/log/certbot.log
-sed -e "s/^domain://g" /usr/local/bin/setting.log |xargs -I {} certbot certonly --standalone --dry-run --expand --register-unsafely-without-email --agree-tos -d {} >> /var/log/certbot.log
-sleep 10
+sed -e "s/^domain://g" /usr/local/bin/setting.log | \
+	xargs -I {} sh -c \
+	'certbot certonly --standalone --dry-run --expand --register-unsafely-without-email --agree-tos -d {} >> /var/log/certbot.log && \
+	sleep 10'
