8fdd6caf0818f4123376fd99196c36e1d0e9d546
kusari-k <45356209+kusari-k@users.noreply.github.com>
2021-02-08 01:52:21 +0900

    add judgment sentence

--- run.sh
+++ run.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/bash
-date >> /var/log/certbot.log
+mkdir -pm 644 /log
+date > /log/certbot.log
 sed -e "s/^domain://g" /usr/local/bin/setting.log | \
 	xargs -I {} sh -c \
-	'certbot certonly --standalone --keep --expand --register-unsafely-without-email --agree-tos -d {} >> /var/log/certbot.log'
-#	'certbot certonly --standalone --dry-run --expand --register-unsafely-without-email --agree-tos -d {} >> /var/log/certbot.log'
+	'certbot certonly --standalone --dry-run --expand --register-unsafely-without-email --agree-tos -d {} >> /log/certbot.log'
+resp=a$(grep "error" /log/certbot.log)
+if [ $resp = "a" ]; then
+	rm -rf /etc/letsencrypt/*
+	echo -e "OK!!\n" >> /log/certbot.log
+	sed -e "s/^domain://g" /usr/local/bin/setting.log | \
+	xargs -I {} sh -c \
+	'certbot certonly --standalone --keep --expand --register-unsafely-without-email --agree-tos -d {} >> /log/certbot.log'
+else
+	echo -e "ERROR!!\n" >> /log/certbot.log
+	echo -e "resp:\n$resp\n"  >> /log/certbot.log
+fi
+echo -e "END"  >> /log/certbot.log
